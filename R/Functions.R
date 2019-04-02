f6c2fcde85435c9c37603711a639dbeb86f6bdcc
Tyler <tyler.w.bradshaw@duke.edu>
2019-04-01 20:45:13 -0400

    add gitstatus:

--- R/Functions.R
+++ R/Functions.R
@@ -1,9 +1,10 @@
+#-------------------------------------------------------------------------------
 #' Creating a file
 #'
-#' Creates a file in the current working directory
+#' Creates a file in the current working directory. Inspired by the linux command `touch`.
 #'
-#' @param file character specifying file that will be created. 
-#' @param dir character expansion for the text
+#' @param file character specifying file that will be created.
+#' @param dir character specifying the working direcotry.
 #'
 #' @return None
 #'
@@ -21,6 +22,34 @@ touch <- function(file = "test.txt", dir = getwd()){
     cmd1 = tolower(substr(dir,1,2)),
     cmd2 = paste("cd",dir),
     cmd3 = paste("echo >>",file)
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
+
+#-------------------------------------------------------------------------------
+#' git status
+#'
+#' Check git status of current directory.
+#'
+#' @param file character specifying the directory of a git repository.
+#'
+#' @return None
+#'
+#' @author Tyler W Bradshaw, \email{tyler.w.bradshaw@duke.edu}
+#' @references \url{https://git-scm.com/docs/git-status}
+#' @keywords git status bash cmd
+#'
+#' @examples
+#' gitstatus()
+#'
+#' @export
+#'
+gitstatus <- function(dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = "git status"
   )
   cmd <- paste(unlist(cmd_list),collapse = " & ")
   shell(cmd)
