11f9c13cd9e3d1f33764e6434cbcde162f1fdb41
twesleyb <tyler.w.bradshaw@duke.edu>
2019-08-07 09:57:50 -0400

    lazy git commit

--- R/Functions.R
+++ R/Functions.R
@@ -190,3 +190,36 @@ gitpush <- function(dir = getwd()){
 }
 
 #-------------------------------------------------------------------------------
+#' write.pajek
+#'
+#' Write network adjacency network to file in Pajek (*.net) format.
+#'
+#' @param adjm (matrix) symmetric adjacency matrix representing the network graph.
+#' @param file (string) name of output file (e.g. 'network.net')
+#'
+#' @return None
+#'
+#' @author Tyler W Bradshaw, \email{tyler.w.bradshaw@duke.edu}
+#' @references \url{https://gephi.org/users/supported-graph-formats/pajek-net-format/}
+#' @keywords network graph pajek write 
+#'
+#' @examples
+#' write.pajek(adjm, "network.net")
+#'
+#' @export
+
+write.pajek <- function(adjm, file, ...) {
+  # Write network adjacency matrix to .net file in Pajek format.
+  # Uses data.table::fwrite for faster performance.
+  colnames(adjm) <- rownames(adjm) <- c(1:ncol(adjm))
+  edge_list <- as.data.table(na.omit(melt(adjm)))
+  colnames(edge_list) <- c("protA","protB","weight")
+  v <- as.data.table(paste(seq(1,ncol(adjm)), " \"", seq(1,ncol(adjm)), "\"", sep = ""))
+  write.table(paste("*Vertices", dim(adjm)[1]), file, 
+              quote = FALSE, row.names = FALSE, col.names = FALSE)
+  fwrite(v, file, quote = FALSE, sep = " ", row.names = FALSE, col.names = FALSE, append = TRUE)
+  write.table("*Edges", file, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
+  fwrite(edge_list, file, sep = " ", col.names = FALSE, append = TRUE)
+}
+
+#-------------------------------------------------------------------------------
