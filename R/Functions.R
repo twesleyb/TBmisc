547e09c49bf079cd6f0df02640c4d41b1ce30e9a
twesleyb <tyler.w.bradshaw@duke.edu>
2019-08-21 11:24:26 -0400

    lazy git commit

--- R/Functions.R
+++ R/Functions.R
@@ -201,25 +201,112 @@ gitpush <- function(dir = getwd()){
 #'
 #' @author Tyler W Bradshaw, \email{tyler.w.bradshaw@duke.edu}
 #' @references \url{https://gephi.org/users/supported-graph-formats/pajek-net-format/}
-#' @keywords network graph pajek write 
+#' @keywords network graph pajek write
 #'
 #' @examples
 #' write.pajek(adjm, "network.net")
 #'
 #' @export
 
-write.pajek <- function(adjm, file, ...) {
+write.pajek <- function(adjm, file = "network.net", col.names = FALSE, ...) {
+  require(data.table, quietly = TRUE)
   # Write network adjacency matrix to .net file in Pajek format.
   # Uses data.table::fwrite for faster performance.
-  colnames(adjm) <- rownames(adjm) <- c(1:ncol(adjm))
+  # Rename columns as numbers.
+  # Node names not currently supported. Causes error loading Pajek file.
+  if (col.names == FALSE) {
+    colnames(adjm) <- rownames(adjm) <- c(1:ncol(adjm))
+  }
   edge_list <- as.data.table(na.omit(melt(adjm)))
   colnames(edge_list) <- c("protA","protB","weight")
-  v <- as.data.table(paste(seq(1,ncol(adjm)), " \"", seq(1,ncol(adjm)), "\"", sep = ""))
-  write.table(paste("*Vertices", dim(adjm)[1]), file, 
-              quote = FALSE, row.names = FALSE, col.names = FALSE)
-  fwrite(v, file, quote = FALSE, sep = " ", row.names = FALSE, col.names = FALSE, append = TRUE)
-  write.table("*Edges", file, quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
+  edge_list <- subset(edge_list, weight > 0 )
+  node_names <- colnames(adjm)
+  v <- as.data.table(paste(seq(1,ncol(adjm)), " \"", node_names, "\"", sep = ""))
+  write.table(paste("*Vertices", dim(adjm)[1]), file,
+              quote = FALSE, sep = " ", row.names = FALSE, col.names = FALSE)
+  fwrite(v, file, quote = FALSE, sep = " ", row.names = FALSE,
+         col.names = FALSE, append = TRUE)
+  write.table("*Edges", file, quote = FALSE, sep = " ",
+              row.names = FALSE, col.names = FALSE, append = TRUE)
   fwrite(edge_list, file, sep = " ", col.names = FALSE, append = TRUE)
 }
 
 #-------------------------------------------------------------------------------
+#' silently
+#'
+#' suppress any unwanted output from a function with sink().
+#'
+#' @param func (function) symmetric adjacency matrix representing the network graph.
+#' @param ... (string) additional arguments passed to func().
+#'
+#' @return None
+#'
+#' @author Tyler W Bradshaw, \email{tyler.w.bradshaw@duke.edu}
+#' @references \url{}
+#' @keywords supress output silent quiet
+#'
+#' @examples
+#' silently(wgcna::bicor, exprDat)
+#'
+#' @export
+## Define a function that can suppress unwanted messages from a function.
+silently <- function(func, ...) {
+  sink(tempfile())
+  out <- func(...)
+  sink(NULL)
+  return(out)
+}
+
+#-------------------------------------------------------------------------------
+#' ggplotScaleFreeFit
+#'
+#' Generate plots examining scale free fit of WGCNA network.
+#'
+#' @param sft (list) output of WGCNA::pickSoftThreshold()
+#'
+#' @return None
+#'
+#' @author Tyler W Bradshaw, \email{tyler.w.bradshaw@duke.edu}
+#' @references \url{}
+#' @keywords ggplot scale free fit pickSoftThreshold
+#'
+#' @examples
+#' ggplotScaleFreeFit(sft)
+#'
+#' @export
+
+ggplotScaleFreeFit <- function(sft) {
+  require(ggplot2, quietly = TRUE)
+  # Gather the data, calculate scale free fit.
+  data <- sft$fitIndices
+  data$fit <- -sign(data$slope) * data$SFT.R.sq
+  # Generate Scale free topology plot.
+  plot1 <- ggplot(data, aes(x = Power, y = fit)) +
+    geom_text(aes(label = Power), color = "red") +
+    ggtitle("Scale independence") +
+    xlab(expression(Soft ~ Threshold ~ Power ~ (beta))) +
+    ylab(expression(Scale ~ Free ~ Topology ~ (R^2))) +
+    geom_hline(yintercept = 0.9, linetype = "dashed", color = "red", size = 0.6) +
+    # geom_hline(yintercept = 0.8, linetype = "dashed", color = "gray", size = 0.6) +
+    theme(
+      plot.title = element_text(hjust = 0.5, color = "black", size = 11, face = "bold"),
+      axis.title.x = element_text(color = "black", size = 11, face = "bold"),
+      axis.title.y = element_text(color = "black", size = 11, face = "bold")
+    )
+  # Generate mean connectivity plot.
+  plot2 <- ggplot(data, aes(x = Power, y = mean.k.)) +
+    geom_text(aes(label = Power), color = "red") +
+    ggtitle("Mean Connectivity") +
+    xlab(expression(Soft ~ Threshold ~ Power ~ (beta))) +
+    ylab(expression(Mean ~ Connectivity ~ (k))) +
+    theme(
+      plot.title = element_text(hjust = 0.5, color = "black", size = 11, face = "bold"),
+      axis.title.x = element_text(color = "black", size = 11, face = "bold"),
+      axis.title.y = element_text(color = "black", size = 11, face = "bold")
+    )
+  plot3 <- plot_grid(plot1, plot2, labels = "auto")
+  data_return <- list(plot1, plot2, plot3)
+  names(data_return) <- c("ScaleFreeFit", "MeanConnectivity", "Grid")
+  return(data_return)
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # TBmiscr
-Repository for R utility functions.
+
+This is a repository for R utility functions.
+
+## Usage
+
+```
+devtools::install_github("twesleyb/TBmiscr")
+```
+
+## Creating an R package.
+
+#### Resources:
+* roxygen format: https://kbroman.org/pkg_primer/pages/docs.html
+* simple tutorial: https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
+* More on the roxygen format: http://r-pkgs.had.co.nz/man.html
+
+## Write r function with roxygen format. 
+* Example: https://github.com/kbroman/pkg_primer/blob/gh-pages/example/stage3/R/plot_crayons.R
+
+```r
+library(devtools)
+library(roxygen2)
+
+devtools::create("package")
+
+# Insure you are in the new packages directory.
+devtools::document()
+
+# Install the package.
+install("cats")
+
+# The package will now be in your library directory.
+# To check where your pacakges are stored:
+# Check where your packages are installed.
+.libPaths()
+
+# You can source the package like normal.
+library("package")
+
+# Once the package is on github, you can install it with devtools::install_github()
+install_github("kbroman/broman") #username/package
+
+```
