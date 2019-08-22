68d8b3633a7282bbdcfe726d4a22a6d2516afb7d
twesleyb <tyler.w.bradshaw@duke.edu>
2019-08-22 12:46:44 -0400

    lazy git commit

--- R/Functions.R
+++ R/Functions.R
@@ -76,6 +76,7 @@ gitpull <- function(dir = getwd()){
     cmd2 = paste("cd",dir),
     cmd3 = "git pull"
   )
+
   cmd <- paste(unlist(cmd_list),collapse = " & ")
   shell(cmd)
 }
@@ -310,3 +311,35 @@ ggplotScaleFreeFit <- function(sft) {
   return(data_return)
 }
 
+#------------------------------------------------------------------------------
+
+#' grobsize
+#'
+#' get the actual height and width of a grob
+#'
+#' @param x (grob) a grob object
+#'
+#' @return None
+#'
+#' @author Tyler W Bradshaw, \email{tyler.w.bradshaw@duke.edu}
+#' @references \url{https://stackoverflow.com/questions/13867325/get-width-of-plot-area-in-ggplot2}
+#' @keywords grob size height width
+#'
+#' @examples
+#' ggsize(grob)
+#'
+#' @export
+
+grobsize <- function(x) {
+	# Function to get absolute size of a grob in inches.
+	# Modified from: Hack-R's solution on Stackoverflow, see refernces.
+	f <- tempfile()
+	png(f)
+	h <- grid::convertHeight(sum(x$heights), "in", TRUE)
+	w <- grid::convertWidth(sum(x$widths), "in", TRUE)
+	dev.off()
+	unlink(f)
+	return(c(w,h))
+}
+
+#------------------------------------------------------------------------------
