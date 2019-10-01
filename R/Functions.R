a8643f35742fe11f2a4a9efe070f3d205aaca254
twesleyb <tyler.w.bradshaw@duke.edu>
2019-10-01 10:42:57 -0400

    lazy git commit

--- R/Functions.R
+++ R/Functions.R
@@ -343,3 +343,72 @@ grobsize <- function(x) {
 }
 
 #------------------------------------------------------------------------------
+
+#' ggplotScaleFreePlot
+#'
+#' get the actual height and width of a grob
+#'
+#' @param x (grob) a grob object
+#'
+#' @return None
+#'
+#' @author Tyler W Bradshaw, \email{tyler.w.bradshaw@duke.edu}
+#' @references \url{https://stackoverflow.com/questions/13867325/get-width-of-plot-area-in-ggplot2}
+#' @keywords grob size height width
+#'
+#' @examples
+#' ggsize(grob)
+#'
+#' @export
+
+
+ggplotScaleFreePlot <- function(connectivity, nBreaks = 10, truncated = FALSE,
+                                removeFirst = FALSE, main = "", ...) {
+  k <- connectivity
+  discretized.k <- cut(k, nBreaks)
+  dk <- tapply(k, discretized.k, mean)
+  p.dk <- as.vector(tapply(k, discretized.k, length) / length(k))
+  breaks1 <- seq(from = min(k), to = max(k), length = nBreaks + 1)
+  hist1 <- suppressWarnings(hist(k,
+    breaks = breaks1, equidist = FALSE,
+    plot = FALSE, right = TRUE
+  )) # ...
+  dk2 <- hist1$mids
+  dk <- ifelse(is.na(dk), dk2, dk)
+  dk <- ifelse(dk == 0, dk2, dk)
+  p.dk <- ifelse(is.na(p.dk), 0, p.dk)
+  log.dk <- as.vector(log10(dk))
+  if (removeFirst) {
+    p.dk <- p.dk[-1]
+    log.dk <- log.dk[-1]
+  }
+  log.p.dk <- as.numeric(log10(p.dk + 1e-09))
+  lm1 <- lm(log.p.dk ~ log.dk)
+  pvalue <- lmp(lm1)
+  print(pvalue)
+
+  title <- paste0(
+    main, " Scale Free R2 =", as.character(round(summary(lm1)$adj.r.squared, 2)),
+    ", slope =", round(lm1$coefficients[[2]], 2)
+  )
+
+  OUTPUT <- data.frame(
+    scaleFreeRsquared = round(summary(lm1)$adj.r.squared, 2),
+    slope = round(lm1$coefficients[[2]], 2)
+  )
+  # Generate ggplot.
+  df <- as.data.frame(cbind(log.dk, log.p.dk))
+  plot <- ggplot(df, aes(x = log.dk, y = log.p.dk)) + geom_point(size = 2) +
+    ggtitle(title) +
+    geom_abline(intercept = coef(lm1)[1], slope = coef(lm1)[2], color = "black", linetype = "dashed") +
+    labs(y = expression(Log[10](p(k)))) +
+    labs(x = expression(Log[10](k))) +
+    theme(
+      plot.title = element_text(hjust = 0.5, color = "black", size = 11, face = "bold"),
+      axis.title.x = element_text(hjust = 0.5, color = "black", size = 11),
+      axis.title.y = element_text(hjust = 0.5, color = "black", size = 11)
+    )
+  out <- list(ggplot = plot, stats = OUTPUT)
+  return(out)
+}
+
