cd0a855c9110c90a22c573182d9967f6745cf8c2
twesleyb <tyler.w.bradshaw@duke.edu>
2020-04-02 10:45:32 -0400

    fix merge error

--- R/notin.R
+++ R/notin.R
@@ -12,13 +12,7 @@
 #'
 #' @import dplyr
 #'
-<<<<<<< HEAD
 #' @examples
 #' x %notin% y
 #' @export
-=======
-#' @export
-#'
-#' @examples
->>>>>>> a7a4942d95e2b37d7595ddc4faecdd3317578351
 `%notin%` <- Negate(`%in%`)
