d9c700d6d9adf420ec752c81c2ed39c8ad0599fd
twesleyb <tyler.w.bradshaw@duke.edu>
2020-04-03 09:01:31 -0400

    add prompt function

---
+++ R/load_all.R
@@ -0,0 +1,7 @@
+load_all <- function(funcdir = file.path(getrd(),"R")) {
+		myfun <- list.files(funcdir, pattern="*\\.R", full.names=TRUE)
+		msg <- do.call(file.path,
+			       as.list(tail(unlist(strsplit(funcdir,"\\/")),2)))
+		message(paste("Loading functions in", msg))
+		invisible({ sapply(myfun,source) })
+}
---
+++ R/prompt.R
@@ -0,0 +1,10 @@
+prompt <- function(prompt=NULL) {
+	# Default prompt.
+	if (is.null(prompt)) { prompt <- "Press <Enter> to continue..." }
+	if (interactive()) {
+		invisible(readline(prompt))
+	} else {
+		cat(prompt)
+		invisible(readLines(file("stdin"), 1))
+	}
+}
