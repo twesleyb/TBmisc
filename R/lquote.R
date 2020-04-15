95abbccb4364f8586fb6d9aa1db57f1ccf17b1b1
twesleyb <tyler.w.bradshaw@duke.edu>
2020-04-15 17:19:04 -0400

    add lquote function

---
+++ R/lquote.R
@@ -0,0 +1,13 @@
+#' lquote
+#' export
+lquote <- function(string,single=TRUE){
+	# Wrap a string in single or double quotes.
+	single_quote = "'"
+	double_quote = "\""
+	if (single) {
+		return(paste0(strwrap(string,prefix=single_quote),single_quote))
+	} else {
+		double = TRUE
+	if (double) { paste0(strwrap(string,prefix=double_quote),double_quote) }
+	}
+}
