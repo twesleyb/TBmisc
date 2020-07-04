69f72bfcc61bf81a805ab77df5a9d574660cf98b
twesleyb <tyler.w.bradshaw@duke.edu>
2020-07-04 10:00:31 -0400

    generate docs

--- R/colormind.R
+++ R/colormind.R
@@ -1,9 +1,14 @@
+#' colormind
+#' @export
 
-# Random colors.
-cmd <- "curl 'http://colormind.io/api/' --silent --data-binary '{\"model\":\"default\"}'"
-response <- system(cmd,intern=TRUE)
-
-result <- rjson::fromJSON(response)
-colors <- unlist(result,recursive=FALSE)
-names(colors) <- gsub("result","color",names(colors))
-
+# Query colormind api for random colors.
+colormind <- function(random=TRUE) {
+	# Query api:
+	cmd <- "curl 'http://colormind.io/api/' --silent --data-binary '{\"model\":\"default\"}'"
+	response <- system(cmd,intern=TRUE)
+	# Parse the response as JSON.
+	result <- rjson::fromJSON(response)
+	colors <- unlist(result,recursive=FALSE) # clean-up
+	names(colors) <- gsub("result","color",names(colors))
+	return(colors)
+}
